d7978d4a3775537a144a599685f866972eb3e261
Henrique Morais <rikeaju@hotmail.com>
2022-07-31 17:27:29 -0300

    Lesson 031 - Vetores, Operações com Vetores e Vetores Nomeados - Parte 02

--- DSA/P01/010.r
+++ DSA/P01/010.r
@@ -2,3 +2,29 @@
 setwd("C:/dev/rikemorais.courses.docs.r/DSA/P01")
 getwd()
 
+# COmbinando Vetores
+v1 = c(2, 3, 5)
+v2 = c("aa", "bb", "cc", "dd", "ee")
+c(v1, v2)
+
+# Operações com Vetores
+x = c(1, 3, 5, 7)
+y = c(2, 4, 6, 8)
+
+x * 5
+x + y
+x - y
+x * y
+x / y
+
+# Somando Vetores com Números Diferentes de Elementos
+alfa = c(10, 20, 30)
+beta = c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+alfa + beta
+
+# Vetor Nomeado
+v = c("Nelson", "Mandela")
+v
+names(v) = c("Nome", "Sobrenome")
+v
+v["Nome"]
